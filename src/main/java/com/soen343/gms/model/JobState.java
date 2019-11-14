b73d77300de4f5d2fe3895f503f56e611feadbc0
Joshua Butler <joshua.butler@mail.concordia.ca>
2019-11-14 16:03:59 -0500

    Feature/revert build to maven (#18)
    * Updated .gitignore file
    
    * Deleted Graddle + IDE specific files. Added pom.xml for Maven Support.
    
    * Updated readme.md to README.md
    
    * Deleted all java source files in project.
    
    * Added spring-boot application
    
    * Added config java classes for custom spring-boot configurations
    
    * Added MVC for User registration
    
    * Added converter for data ORM persistence of enums
    
    * Added bare bones html templates for a quick and dirty UI
    
    * Added JobState Enum for state pattern implementation

---
+++ src/main/java/com/soen343/gms/model/JobState.java
@@ -0,0 +1,57 @@
+package com.soen343.gms.model;
+
+public enum JobState {
+    Initial {
+        @Override
+        public JobState nextState() { return InProgress; }
+        @Override
+        public JobState prevState() { return this; }
+        @Override
+        public String getCode() { return "I"; }
+    },
+    InProgress {
+        @Override
+        public JobState nextState() { return PendingApproval; }
+        @Override
+        public JobState prevState() { return this; }
+        public JobState onHold() { return OnHold; }
+        @Override
+        public String getCode() { return "IP"; }
+    },
+    OnHold {
+        @Override
+        public JobState nextState() { return InProgress; }
+        @Override
+        public JobState prevState() { return InProgress; }
+        @Override
+        public String getCode() { return "H"; }
+    },
+    PendingApproval {
+        @Override
+        public JobState nextState() { return Complete; }
+        @Override
+        public JobState prevState() { return InProgress; }
+        @Override
+        public String getCode() { return "PA"; }
+    },
+    Complete {
+        @Override
+        public JobState nextState() { return Archived; }
+        @Override
+        public JobState prevState() { return this; }
+        @Override
+        public String getCode() { return "C"; }
+    },
+    Archived {
+        @Override
+        public JobState nextState() { return this; }
+        @Override
+        public JobState prevState() { return this; }
+        @Override
+        public String getCode() { return "A"; }
+    };
+
+    public abstract JobState nextState();
+    public abstract JobState prevState();
+    public abstract String getCode();
+}
