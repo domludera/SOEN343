4ec503de233b6e67a822a71cd29ba03a38f98f6a
MIke <m_calix@encs.concordia.ca>
2019-11-27 22:41:37 -0500

    MVCConfig Correction

--- src/main/java/com/soen343/gms/config/MvcConfig.java
+++ src/main/java/com/soen343/gms/config/MvcConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.format.FormatterRegistry;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -14,20 +13,7 @@ public class MvcConfig implements WebMvcConfigurer {
     public void addFormatters(FormatterRegistry registry) {
         registry.addConverterFactory(new StringToEnumConverterFactory());
     }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler(
-                "/image/**",
-                "/css/**",
-                "/js/**")
-                .addResourceLocations(
-                        "classpath:/static/image/",
-                        "classpath:/static/css/",
-                        "classpath:/static/js/");
-    }
-
-
+    
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
         BCryptPasswordEncoder bCryptPasswordEncoder = new BCryptPasswordEncoder();
